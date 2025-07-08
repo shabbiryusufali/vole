3115fc16de1580199327140ff5e626b2a3829267
Callum Barker <64287849+cbarkr@users.noreply.github.com>
2025-07-08 07:38:56 +0000

    3 design cli interface (#9)
    * implement basic base for command pattern
    
    * add abstract properties to module abstract base class
    
    * add setters to base module
    
    * scaffold basic CLI functionality
    
    * fix error in readme
    
    * actually USE the project and CFG
    
    * format
    
    * split parser into separate file
    
    * execute should return something!

---
+++ vole/cli.py
@@ -0,0 +1,46 @@
+import argparse
+import logging
+import pathlib
+
+
+log_levels = {
+    "debug": logging.DEBUG,
+    "info": logging.INFO,
+    "warning": logging.WARNING,
+    "error": logging.ERROR,
+    "critical": logging.CRITICAL,
+}
+
+defaults = {"output": "./output", "verbosity": "info"}
+
+
+def parse() -> dict:
+    parser = argparse.ArgumentParser(
+        prog="vole.py",
+        description="""
+            Vulnerability Observance and Learning-based Exploitation (VOLE)
+            is a tool for detecting important CWEs in program binaries
+        """,
+    )
+
+    # Positional
+    parser.add_argument("binary", type=pathlib.Path)
+
+    # Options
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=pathlib.Path,
+        default=defaults.get("output"),
+        help="Where to output the results",
+    )
+    parser.add_argument(
+        "-vvv",
+        "--verbosity",
+        type=str,
+        default=defaults.get("verbosity"),
+        choices=list(log_levels.keys()),
+        help="How much log detail to output",
+    )
+
+    return vars(parser.parse_args())
---
+++ vole/modules/__init__.py
@@ -0,0 +1,4 @@
+# TODO: Import modules here
+
+# TODO: Insert imported modules into `modules`
+modules = []
