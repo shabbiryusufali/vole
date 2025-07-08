756f45d24563cadbeaf0f413cbd3aecce1841881
Callum Barker <64287849+cbarkr@users.noreply.github.com>
2025-07-08 07:37:11 +0000

    2 design module interface (#7)
    * implement basic base for command pattern
    
    * add abstract properties to module abstract base class
    
    * add setters to base module
    
    * format
    
    * execute should return something!

---
+++ vole/modules/abstract.py
@@ -0,0 +1,34 @@
+from abc import ABC, abstractmethod
+
+
+class Module(ABC):
+    """
+    Declares properties and methods for executing a module
+    """
+
+    @property
+    @abstractmethod
+    def cwe_id(self):
+        pass
+
+    @property
+    @abstractmethod
+    def cwe_name(self):
+        pass
+
+    @property
+    @abstractmethod
+    def description(self):
+        pass
+
+    @abstractmethod
+    def set_project(self, project):
+        pass
+
+    @abstractmethod
+    def set_CFG(self, project):
+        pass
+
+    @abstractmethod
+    def execute(self):
+        pass
