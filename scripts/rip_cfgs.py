900af6af2b85722c719f5085283975750e61dd1d
cbarkr <cbarkr@gmail.com>
2025-08-03 13:10:24 -0700

    add fix to make subgraphs compatible with `torch_geometric.utils.convert.from_networkx`

--- scripts/rip_cfgs.py
+++ scripts/rip_cfgs.py
@@ -28,6 +28,19 @@ def crawl(path: pathlib.Path, pattern: str) -> Generator[pathlib.Path]:
 def lift(cwe_id: str, file: pathlib.Path) -> Generator[networkx.DiGraph]:
     """
     Resolves subgraphs of CFG corresponding to test cases
+
+    The edges of each subgraph must be normalized to play nicely with
+    `torch_geometric.utils.convert.from_networkx`.
+
+    Why?
+        1. `pyvex` inconsistently assigns edge attributes; sometimes edges are
+            missing `stmt_idx`
+        2. `torch_geometric.utils.convert.from_networkx` determines which edge
+            attributes are "standard" by inspecting the last edge in the graph,
+            then checks that all other edges have the same attributes
+
+    To address this, we preemptively perform the same check, and standardize
+    any identified difference
     """
     # Load each source binary
     for o in crawl(file.parent, f"**/{cwe_id}*.o"):
@@ -42,11 +55,43 @@ def lift(cwe_id: str, file: pathlib.Path) -> Generator[networkx.DiGraph]:
         )
 
         # The CFG contains a weakly connected component for each test case
-        # Extract the subgraphs
         wcc = networkx.weakly_connected_components(src_cfg.model.graph)
         sub_cfgs = [src_cfg.model.graph.subgraph(i).copy() for i in wcc]
 
         for sub_cfg in sub_cfgs:
+            try:
+                edge_attrs = set(
+                    list(next(iter(sub_cfg.edges(data=True)))[-1].keys())
+                )
+                for idx, (u, v, attr_dict) in enumerate(
+                    sub_cfg.edges(data=True)
+                ):
+                    key_set = set(attr_dict.keys())
+                    if key_set != edge_attrs:
+                        new_attr_dict = attr_dict
+
+                        if len(key_set) < len(edge_attrs):
+                            # Often, edges with `jumpkind = Ijk_Boring` lack `stmt_idx`
+                            # Those with `stmt_idx` seem to always have it set to -2
+                            for diff in edge_attrs.difference(key_set):
+                                new_attr_dict[diff] = -2
+
+                        elif len(key_set) > len(edge_attrs):
+                            # Removes the offending difference
+                            # TODO: Fix last edge against which all others are compared
+                            for diff in key_set.difference(edge_attrs):
+                                del new_attr_dict[diff]
+
+                        # Update edge with new attributes
+                        networkx.set_edge_attributes(
+                            sub_cfg, {(u, v): new_attr_dict}
+                        )
+
+            except StopIteration:
+                # NOTE: CWE-703!
+                # TODO: Handle this in a less hacky fashion
+                pass  # nosec B110
+
             yield sub_cfg
 
 
