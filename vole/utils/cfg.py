0263c5282d57fc23f5d27dc0eab77a5c120af98c
cbarkr <cbarkr@gmail.com>
2025-08-04 01:36:40 -0700

    add CFG utils

---
+++ vole/utils/cfg.py
@@ -0,0 +1,60 @@
+import angr
+import pathlib
+import networkx as nx
+
+from .graph import (
+    traverse_digraph,
+    extract_subgraphs,
+    normalize_edge_attributes,
+    insert_node_attributes,
+)
+
+from angr.knowledge_plugins.cfg import CFGNode
+from collections.abc import Iterator
+
+
+def lift_ir(cfg: nx.DiGraph) -> tuple[CFGNode, str]:
+    """
+    Iterator that yields the IR of each `CFGNode` in `cfg`
+    """
+    for node in traverse_digraph(cfg):
+        if node.block:
+            yield (node, str(node.block.vex))
+
+
+def get_program_cfg(file: pathlib.Path):
+    """
+    Returns the CFG of `file`
+    """
+    project = angr.Project(file, auto_load_libs=False)
+    cfg = project.analyses.CFGFast(
+        # Force complete scan to get as much information as possible
+        force_complete_scan=True,
+        force_smart_scan=False,
+        resolve_indirect_jumps=True,
+        normalize=True,
+    )
+
+    return cfg
+
+
+def get_sub_cfgs(cfg: nx.DiGraph) -> Iterator[nx.DiGraph]:
+    """
+    Iterator that yields a `nx.DiGraph` corresponding to a subgraph of `cfg`
+    """
+    for sub_cfg in extract_subgraphs(cfg.model.graph):
+        normalize_edge_attributes(sub_cfg)
+
+        yield sub_cfg
+
+
+def vectorize_node_ir(cfg: nx.DiGraph) -> None:
+    """
+    Converts the IR for each node in the `cfg` to a vector representation and inserts it into the node as an attribute
+    """
+    node_attrs = {k: v for k, v in lift_ir(cfg)}
+
+    # TODO: Convert node_attrs.values() to vectors
+    # NOTE: See utils/train.py
+
+    insert_node_attributes(cfg, "ir_vec", node_attrs)
