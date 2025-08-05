6541190447bc7ba062145bf481e463b4ddb58422
cbarkr <cbarkr@gmail.com>
2025-08-05 12:49:25 -0700

    update training script to account for latest changes

--- vole/train.py
+++ vole/train.py
@@ -3,7 +3,7 @@ import pathlib
 import argparse
 
 from utils.cfg import get_program_cfg, get_sub_cfgs, lift_stmt_ir, vectorize_stmt_ir
-from utils.graph import get_digraph_source_node, insert_node_attributes, to_torch_data
+from utils.graph import get_digraph_source_nodes, insert_node_attributes, to_torch_data
 from utils.train import get_corpus_splits
 
 # Silence angr
@@ -24,21 +24,21 @@ def prepare_training_data(split: list[pathlib.Path]) -> list:
         cfg = get_program_cfg(path)
 
         for sub_cfg in get_sub_cfgs(cfg):
-            source = get_digraph_source_node(sub_cfg)
+            source = get_digraph_source_nodes(sub_cfg)[0]
+            name = sub_cfg.nodes[source].get("name")
 
             # TODO: More granular labelling
-            if not source or not source.name:
-                label = -1 # Invalid
-            elif "bad" in source.name:
-                label = 0 # Bad (i.e. vulnerable)
-            elif "good" in source.name:
-                label = 1 # Good
+            if not name:
+                label = -1  # Invalid
+            elif "bad" in name:
+                label = 0  # Bad (i.e. vulnerable)
+            elif "good" in name:
+                label = 1  # Good
 
             for node, stmts_ir in lift_stmt_ir(sub_cfg):
-                stmts_vec = vectorize_stmt_ir(stmts_ir)
-
                 # Insert features as node attributes
                 # This ensures the values are preserved by torch later
+                stmts_vec = vectorize_stmt_ir(stmts_ir)
                 insert_node_attributes(sub_cfg, {node: {"label": label}})
                 insert_node_attributes(sub_cfg, {node: {"ir_vec": stmts_vec}})
 
