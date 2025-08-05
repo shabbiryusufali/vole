34ea74e403bf6393edb657efcf98f74afe180f75
cbarkr <cbarkr@gmail.com>
2025-08-05 01:59:14 -0700

    scaffold `train.py`, the accumulation of everything thus far

---
+++ vole/train.py
@@ -0,0 +1,90 @@
+import logging
+import pathlib
+import argparse
+
+from utils.cfg import get_program_cfg, get_sub_cfgs, lift_stmt_ir, vectorize_stmt_ir
+from utils.graph import get_digraph_source_node, insert_node_attributes, to_torch_data
+from utils.train import get_corpus_splits
+
+# Silence angr
+logger = logging.getLogger("cle.backends.externs")
+logger.setLevel(logging.ERROR)
+
+logger = logging.getLogger("cle.loader")
+logger.setLevel(logging.ERROR)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+def prepare_training_data(split: list[pathlib.Path]) -> list:
+    training_data = []
+
+    for idx, path in enumerate(split):
+        cfg = get_program_cfg(path)
+
+        for sub_cfg in get_sub_cfgs(cfg):
+            source = get_digraph_source_node(sub_cfg)
+
+            # TODO: More granular labelling
+            if not source or not source.name:
+                label = -1 # Invalid
+            elif "bad" in source.name:
+                label = 0 # Bad (i.e. vulnerable)
+            elif "good" in source.name:
+                label = 1 # Good
+
+            for node, stmts_ir in lift_stmt_ir(sub_cfg):
+                stmts_vec = vectorize_stmt_ir(stmts_ir)
+
+                # Insert features as node attributes
+                # This ensures the values are preserved by torch later
+                insert_node_attributes(sub_cfg, {node: {"label": label}})
+                insert_node_attributes(sub_cfg, {node: {"ir_vec": stmts_vec}})
+
+            training_data.append(to_torch_data(sub_cfg))
+
+    return training_data
+
+
+def train_gcn(cwe_id: str, path: pathlib.Path):
+    """
+    Trains a `torch_geometric.nn.models.GCN` from SARD test case binaries
+    matching `cwe_id` in `path`
+    """
+    train, test, evaluation = get_corpus_splits(cwe_id, path)
+
+    if not all((train, test, evaluation)):
+        logger.info(
+            f"""
+            CWE-ID `{cwe_id}` and path `{path}` yielded no results.
+            Check that `path` contains the compiled test cases.
+            """
+        )
+
+    training_data = prepare_training_data(train)
+
+    # TODO: Train GCN on `training_data`
+
+
+def parse() -> dict:
+    parser = argparse.ArgumentParser(
+        prog="train.py",
+        description="""
+            Train a PyTorch neural network on SARD test cases
+        """,
+    )
+
+    parser.add_argument("CWE-ID", type=str)
+    parser.add_argument("path", type=pathlib.Path)
+
+    return vars(parser.parse_args())
+
+
+def main():
+    args = parse()
+    train_gcn(args.get("CWE-ID"), args.get("path"))
+
+
+if __name__ == "__main__":
+    main()
