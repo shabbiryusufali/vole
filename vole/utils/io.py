3511171271691f17d11e18cb43cd36692e099a71
cbarkr <cbarkr@gmail.com>
2025-08-09 16:41:34 -0700

    move get_corpus_splits to io utils and fix signature

--- vole/utils/io.py
+++ vole/utils/io.py
@@ -1,4 +1,6 @@
 import pathlib
+import numpy as np
+
 from collections.abc import Iterator
 
 
@@ -8,3 +10,23 @@ def crawl(path: pathlib.Path, pattern: str) -> Iterator[pathlib.Path]:
     """
     for file in path.rglob(pattern):
         yield file
+
+
+def get_corpus_splits(
+    cwe_id: str, path: pathlib.Path
+) -> tuple[list, list, list]:
+    """
+    Reads in paths of test files from `path` corresponding to `cwe_id`,
+    shuffles them, and returns a 50-50 train-test split
+    """
+    full = []
+    for m in crawl(path, f"{cwe_id}*/**/main_linux.o"):
+        for c in crawl(m.parent, f"**/{cwe_id}*.o"):
+            full.append(c)
+
+    full = np.array(full)
+    np.random.shuffle(full)
+    full = full.tolist()
+
+    half = len(full) // 2
+    return full[:half], full[half:]
