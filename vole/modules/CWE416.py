08620d9825f644481b785f6fa604ee8d3e1c87c7
cbarkr <cbarkr@gmail.com>
2025-08-10 13:52:40 -0700

    add model tuned by optuna (it still sucks)

--- vole/modules/CWE416.py
+++ vole/modules/CWE416.py
@@ -1,3 +1,4 @@
+import json
 import torch
 import pathlib
 
@@ -12,15 +13,20 @@ PARENT = pathlib.Path(__file__).parent.resolve()
 class CWE416(Module):
     def __init__(self, project, cfg, device, embeddings):
         super().__init__(project, cfg, device, embeddings)
-        self.path = pathlib.Path(PARENT / "../models/CWE416.model")
+        self.param_path = pathlib.Path(PARENT / "../models/CWE416/CWE416.json")
+        self.model_path = pathlib.Path(PARENT / "../models/CWE416/CWE416.model")
+        
+        with open(self.param_path, "r") as f:
+            self.params = json.load(f)
+
         self.model = GCN(
             in_channels=116,
             out_channels=2,
-            hidden_channels=64,
-            num_layers=3,
-            add_self_loops=False,
+            hidden_channels=self.params.get("hidden_channels"),
+            num_layers=self.params.get("num_layers"),
+            dropout=self.params.get("dropout")
         )
-        self.model.load_state_dict(torch.load(self.path, weights_only=True))
+        self.model.load_state_dict(torch.load(self.model_path, weights_only=True))
         self.model.to(self.device)
         self.model.eval()
 
