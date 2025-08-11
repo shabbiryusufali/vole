6af9c3106a74dedf70e3a3f981bcd23809b25b74
Shabbir Yusufali <sya167@sfu.ca>
2025-08-11 12:42:04 -0700

    Add CWE476

---
+++ vole/modules/CWE476.py
@@ -0,0 +1,44 @@
+import json
+import torch
+import pathlib
+
+from .abstract import Module
+
+from torch_geometric.nn.models import GCN
+
+PARENT = pathlib.Path(__file__).parent.resolve()
+
+
+class CWE476(Module):
+    def __init__(self, project, cfg, device, embeddings):
+        super().__init__(project, cfg, device, embeddings)
+        self.param_path = pathlib.Path(PARENT / "../models/CWE476/CWE476.json")
+        self.model_path = pathlib.Path(PARENT / "../models/CWE476/CWE476.model")
+
+        with open(self.param_path, "r") as f:
+            self.params = json.load(f)
+
+        self.model = GCN(
+            in_channels=116,
+            out_channels=2,
+            hidden_channels=self.params.get("hidden_channels"),
+            num_layers=self.params.get("num_layers"),
+            dropout=self.params.get("dropout"),
+        )
+        self.model.load_state_dict(
+            torch.load(self.model_path, weights_only=True)
+        )
+        self.model.to(self.device)
+        self.model.eval()
+
+    @property
+    def cwe_id(self):
+        return "CWE476"
+
+    @property
+    def cwe_name(self):
+        return "NULL Pointer Dereference"
+
+    @property
+    def description(self):
+        return "Dereferencing a pointer that is NULL."
--- vole/modules/__init__.py
+++ vole/modules/__init__.py
@@ -2,6 +2,7 @@
 from .CWE122 import CWE122
 from .CWE123 import CWE123
 from .CWE416 import CWE416
+from .CWE476 import CWE476
 
 # TODO: Insert imported modules into `modules`
-modules = [CWE122, CWE123, CWE416]
+modules = [CWE122, CWE123, CWE416, CWE476]
