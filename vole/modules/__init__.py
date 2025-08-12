57e11348e8110aec4c52ba2fa1bfb972bc57069d
acey <adevjiani@gmail.com>
2025-08-12 06:36:54 -0700

    added CWE121 model

--- vole/modules/__init__.py
+++ vole/modules/__init__.py
@@ -1,8 +1,9 @@
 # TODO: Import modules here
+from .CWE121 import CWE121
 from .CWE122 import CWE122
 from .CWE123 import CWE123
 from .CWE416 import CWE416
 from .CWE476 import CWE476
 
 # TODO: Insert imported modules into `modules`
-modules = [CWE122, CWE123, CWE416, CWE476]
+modules = [CWE121, CWE122, CWE123, CWE416, CWE476]
