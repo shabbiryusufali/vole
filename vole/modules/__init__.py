f158fa635eb0a83d434b24336b5942c0d435533b
Shabbir Yusufali <sya167@sfu.ca>
2025-08-13 02:11:57 -0700

    Retrain CWE 832

--- vole/modules/__init__.py
+++ vole/modules/__init__.py
@@ -4,6 +4,7 @@ from .CWE122 import CWE122
 from .CWE123 import CWE123
 from .CWE416 import CWE416
 from .CWE476 import CWE476
+from .CWE832 import CWE832
 
 # TODO: Insert imported modules into `modules`
-modules = [CWE121, CWE122, CWE123, CWE416, CWE476]
+modules = [CWE121, CWE122, CWE123, CWE416, CWE476, CWE832]
